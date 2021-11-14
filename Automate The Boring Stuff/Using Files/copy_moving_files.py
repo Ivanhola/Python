d5853739a82dee55bc67eccdeb29f23c153480a9
Ivanhola <ivanholalshs@gmail.com>
2021-11-13 16:17:32 -0800

    Added copying and moving example

---
+++ Automate The Boring Stuff/Using Files/copy_moving_files.py
@@ -0,0 +1,6 @@
+import shutil
+#will copy copy_test into the folder
+shutil.copy("C:\\Users\\ivanh\Desktop\\Code\\Python\\Automate The Boring Stuff\\Using Files\\copy_test.txt", "C:\\Users\\ivanh\Desktop\\Code\\Python\\Automate The Boring Stuff\\Using Files\\copy_test_folder")
+
+#You can move files using .move() 
+#shutil.move("C:\\Users\\ivanh\Desktop\\Code\\Python\\Automate The Boring Stuff\\Using Files\\copy_test.txt", "C:\\Users\\ivanh\Desktop\\Code\\Python\\Automate The Boring Stuff\\Using Files\\copy_test_folder")
