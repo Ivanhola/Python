9715e69946cb87f3a03972387ed4ce9652eb907e
ivanhola <ivanholalshs@gmail.com>
2021-11-26 00:13:27 -0800

    fixed bug

--- Program examples/Web Mapping/web mapping with python.py
+++ Program examples/Web Mapping/web mapping with python.py
@@ -7,21 +7,19 @@ import pandas
 os.chdir(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) #this gets the current location reliably
 fileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-map = folium.Map(location=[38.55,98.08], zoom_start=6, tiles = "Stamen Terrain")
+map = folium.Map(location=[39.01,-98.48], zoom_start=3)
 
 fg = folium.FeatureGroup(name="My Map")
 #For loop to insert coordinates
 volcanoCoords = pandas.read_csv(os.path.join(fileLocation, "Volcanoes.txt"))
 
-volcanoesLAT= list(volcanoCoords.loc[:,"LAT"])
-volcanoesLON= list(volcanoCoords.loc[:,"LON"])
-
+volcanoesLAT= list(volcanoCoords["LAT"])
+volcanoesLON= list(volcanoCoords["LON"])
+elevationData= list(volcanoCoords["ELEV"])
 
 #Nested for loop getting lat and long
-for lat in volcanoesLAT:
-    for lon in volcanoesLON:
-        
-        fg.add_child(folium.Marker(location=[lat,lon], popup="I'm a marker", icon=folium.Icon(color="green")))
+for lat, lon, el in zip(volcanoesLAT,volcanoesLON, elevationData):
+    fg.add_child(folium.Marker(location=[lat,lon], popup=str(el) + " m.", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
